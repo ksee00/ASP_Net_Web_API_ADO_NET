e589110673aadfd1fb38b29280010f63b84f7dfa
Koksin Developer <ksee00@hotmail.com>
2019-02-08 12:08:22 +0800

    sss

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<employeesData> Get()
+        {
+            using (TESTEntities entities = new TESTEntities())
+            {
+                return entities.employeesDatas.ToList();
+            }
+        }
+
+        public employeesData Get(int id)
+        {
+            using (TESTEntities entities = new TESTEntities())
+            {
+                return entities.employeesDatas.FirstOrDefault(e => e.Id == id);
+
+            }
+        }
+    }
+}
